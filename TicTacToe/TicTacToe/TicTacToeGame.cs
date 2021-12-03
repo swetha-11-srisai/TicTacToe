ed30b83be9b925d04bb8a31b800bb488f7709fc5
swetha-11-srisai <swethachowdary857@gmail.com>
2021-12-03 11:26:41 -0800

    [Swetha] add :show board

--- TicTacToe/TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToe/TicTacToeGame.cs
@@ -11,7 +11,7 @@ namespace TicTacToe
         
         public void Check()
         {
-            while (player < arr.Length)
+            while (arr[player] < arr.Length)
             {
                 player++;
             }
@@ -24,6 +24,18 @@ namespace TicTacToe
             {
                 Console.WriteLine("Player 1 chance");
             }
+        }
+        public void ShowMethod()
+        {
+            Console.WriteLine("| | ");
+            Console.WriteLine("{0} | {1} |{2}",arr[1],arr[2],arr[3]);
+            Console.WriteLine("__|__|__");
+            Console.WriteLine("| | ");
+            Console.WriteLine("{0} | {1} |{2}", arr[4], arr[5], arr[6]);
+            Console.WriteLine("__|__|__");
+            Console.WriteLine("| | ");
+            Console.WriteLine("{0} | {1} |{2}", arr[7], arr[8], arr[9]);
+            Console.WriteLine("| | ");
         }
 
     }
